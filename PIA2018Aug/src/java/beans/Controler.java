d8ad354d415a2496296fca9ce557e2349c2f72cb
abster95 <gicic.abdulah@gmail.com>
2018-08-11 19:46:35 +0200

    Add default view for non logged in user

--- PIA2018Aug/src/java/beans/Controler.java
+++ PIA2018Aug/src/java/beans/Controler.java
@@ -7,6 +7,8 @@ package beans;
 
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
@@ -18,23 +20,36 @@ import javax.faces.context.FacesContext;
 @ManagedBean
 @SessionScoped
 public class Controler implements Serializable {
+
     private User user;
-    
-    public String logIn(){
+    private List<InterCityLine> interCityLines;
+    private List<InterCityLine> filteredInterCityLines;
+
+    public String logIn() {
         return null;
     }
-    
-    public String register(){
+
+    public String register() {
         return "register";
     }
-    public String checkAndRegister(){
+
+    public String checkAndRegister() {
         return null;
     }
+
+    public String showDefaultPage() {
+        return "default";
+    }
+
     /**
      * Creates a new instance of Controler
      */
     public Controler() {
         user = new User();
+        interCityLines = new ArrayList<>();
+        //FIXME: query the db for the interCityLines
+        interCityLines.add(new InterCityLine("Busko", "00:00", "01:00", "Beograd", "Novi Pazar", "Beograd#Kragujevac#Kraljevo#Novi Pazar"));
+        interCityLines.add(new InterCityLine("AutoBUS", "00:30", "15:00", "Beograd", "Subotica", "Beograd#Novi Sad#Backa Topola#Subotica"));
     }
 
     public User getUser() {
@@ -44,5 +59,21 @@ public class Controler implements Serializable {
     public void setUser(User user) {
         this.user = user;
     }
-    
+
+    public List<InterCityLine> getInterCityLines() {
+        return interCityLines;
+    }
+
+    public void setInterCityLines(List<InterCityLine> interCityLines) {
+        this.interCityLines = interCityLines;
+    }
+
+    public List<InterCityLine> getFilteredInterCityLines() {
+        return filteredInterCityLines;
+    }
+
+    public void setFilteredInterCityLines(List<InterCityLine> filteredInterCityLines) {
+        this.filteredInterCityLines = filteredInterCityLines;
+    }
+
 }
--- PIA2018Aug/src/java/beans/InterCityLine.java
+++ PIA2018Aug/src/java/beans/InterCityLine.java
@@ -10,11 +10,76 @@ package beans;
  * @author Abi
  */
 public class InterCityLine {
+    private String busCompany;
+    private String startTime;
+    private String endTime;
+    private String firstStation;
+    private String lastStation;
+    private String otherStations;
 
+    public InterCityLine(String busCompany, String startTime, String endTime, String firstStation, String lastStation, String otherStations) {
+        this.busCompany = busCompany;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.firstStation = firstStation;
+        this.lastStation = lastStation;
+        this.otherStations = otherStations;
+    }
+    
     /**
      * Creates a new instance of InterCityLine
      */
     public InterCityLine() {
     }
 
+    public String getBusCompany() {
+        return busCompany;
+    }
+
+    public void setBusCompany(String busCompany) {
+        this.busCompany = busCompany;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getFirstStation() {
+        return firstStation;
+    }
+
+    public void setFirstStation(String firstStation) {
+        this.firstStation = firstStation;
+    }
+
+    public String getLastStation() {
+        return lastStation;
+    }
+
+    public void setLastStation(String lastStation) {
+        this.lastStation = lastStation;
+    }
+
+    public String getOtherStations() {
+        String tmp = otherStations;
+        return tmp.replace('#', ' ');
+    }
+
+    public void setOtherStations(String otherStations) {
+        this.otherStations = otherStations;
+    }
+
+    
 }
